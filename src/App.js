a30746f1bd97b11900f3d66d191391bedcd387c0
Tkachenko Anton <tkachenko.anton.90@gmail.com>
2021-02-27 18:24:58 +0200

    add a basic solution to the first task

--- src/App.js
+++ src/App.js
@@ -1,7 +1,58 @@
-import React from "react";
+import React, { Component } from "react";
 
-const App = () => {
-  return <div>Hi</div>;
-};
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  hendelGoodStateChange = () =>
+    this.setState((prevState) => {
+      return { good: prevState.good + 1 };
+    });
+
+  hendelNeutralStateChange = () =>
+    this.setState((prevState) => {
+      return { neutral: prevState.neutral + 1 };
+    });
+
+  hendelBadStateChange = () =>
+    this.setState((prevState) => {
+      return { bad: prevState.bad + 1 };
+    });
+
+  countTotalFeedback = () => {
+    return this.state.good + this.state.neutral + this.state.bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    return Math.floor((this.state.good / this.countTotalFeedback()) * 100);
+  };
+
+  render() {
+    const totalVotes = this.countTotalFeedback();
+    const positiveFeedbackPercentage = this.countPositiveFeedbackPercentage();
+    return (
+      <>
+        <h1>Please leave feedback</h1>
+        <div>
+          <button onClick={this.hendelGoodStateChange}>Good</button>
+          <button onClick={this.hendelNeutralStateChange}>Neutral</button>
+          <button onClick={this.hendelBadStateChange}>Bad</button>
+        </div>
+        <div>
+          <p>Good: {this.state.good}</p>
+          <p>Neutral: {this.state.neutral}</p>
+          <p>Bad: {this.state.bad}</p>
+        </div>
+        <div>
+          <p>Total: {totalVotes}</p>
+          <p>Positive feedback: {positiveFeedbackPercentage}</p>
+        </div>
+      </>
+    );
+  }
+}
 
 export default App;
